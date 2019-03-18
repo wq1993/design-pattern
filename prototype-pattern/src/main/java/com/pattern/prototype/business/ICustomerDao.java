ae75edf79c7b6ea0d73e13355663c742d8ad9694
wq1993 <1647208224@qq.com>
2019-03-18 23:35:41 +0800

    删除错误的提交：prototype-pattern

--- prototype-pattern/src/main/java/com/pattern/prototype/business/ICustomerDao.java
+++
@@ -1,8 +0,0 @@
-package com.pattern.prototype.business;
-
-/**
- * Created by wq on 2019/3/18
- */
-public interface ICustomerDao {
-    void updateCustomerByPrimaryKey(CustomerPo customer);
-}
--- prototype-pattern/src/main/java/com/pattern/prototype/business/ICustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.pattern.prototype.business;
-
-/**
- * Created by wq on 2019/3/18
- */
-public interface ICustomerService {
-
-    boolean updateCustomerByPrimaryKey(CustomerVo customerVo);
-}
